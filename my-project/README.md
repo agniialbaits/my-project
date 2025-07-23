21d2488ef5e03c3ebbe328e439f8dc3443c3cc9c
Agnia Albaitsah <agnialatas1@gmail.com>
2025-07-23 11:29:08 +0700

    update

---
+++ my-project/README.md
@@ -0,0 +1,14 @@
+- app.jsx (index atau induk dari semua page yang ada)
+- index.css (style untuk app.jsx)
+
+
+<!-- FOLDER COMPONENTS PENJELASAN -->
+Folder Components
+mencangkup semua component untuk dimasukan ke pages
+contoh : 
+homePage.jsx hanya untuk parentnya saja
+komponennya berada di Folder "Components"
+misalnya 
+-navbar
+-hero
+-dll
---
+++ my-project/src/Components/layananKami/LayananKami.jsx
@@ -0,0 +1,11 @@
+const LayananKami = () => {
+    return (
+        <div className="layanan-kami">
+            <h1>Layanan Kami</h1>
+            <p>Di sini Anda dapat menemukan berbagai layanan yang kami tawarkan untuk memenuhi kebutuhan Anda.</p>
+            {/* Tambahkan konten layanan di sini */}
+        </div>
+    );
+}
+
+export default LayananKami;
